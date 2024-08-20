38e344e7db897485aecf692b43b0d3ed9a5b64ea
wkywky123123 <89382167+wkywky123123@users.noreply.github.com>
2024-08-20 19:46:25 +0800

    Update Page404.tsx

--- components/Page404.tsx
+++ components/Page404.tsx
@@ -6,7 +6,7 @@ import { PageHead } from './PageHead'
 import styles from './styles.module.css'
 
 export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
-  const title = site?.name || 'Notion Page Not Found'
+  const title = site?.name || '404'
 
   return (
     <>
@@ -14,22 +14,21 @@ export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
 
       <div className={styles.container}>
         <main className={styles.main}>
-          <h1>Notion Page Not Found</h1>
+          <h1>404</h1>
 
           {error ? (
             <p>{error.message}</p>
           ) : (
             pageId && (
               <p>
-                Make sure that Notion page &quot;{pageId}&quot; is publicly
-                accessible.
+               啊哦，你要找的页面不存在了~
               </p>
             )
           )}
 
           <img
             src='/404.png'
-            alt='404 Not Found'
+            alt='404找不到页面'
             className={styles.errorImage}
           />
         </main>
