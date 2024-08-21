151baff1131314f134d9f8c148ea3929fc51a120
Mr忾氧 <1273946770@qq.com>
2024-08-21 10:12:11 +0800

    应该能用

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'b89dc7c4fb4c41938ec63ce8637c2205?pvs=4',
+  rootNotionPageId: '767a479a9f9844a3b8d9635ad071a8d7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
